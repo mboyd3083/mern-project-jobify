8096246c67a0900a2c7f7bfffc220f89e4b646f8
Mike Boyd <mboyd3083@gmail.com>
2023-03-17 11:17:21 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+
+
 function App() {
   return (
     <div className="App">
     <h1>hello</h1>
+    <section>
+      <h2>my article</h2>
+      <article>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus quae sapiente soluta adipisci, dolores, provident nam ipsum illum, laudantium dolorum id perspiciatis accusantium. Provident sapiente fugiat dignissimos, cum quos earum.
+      </article>
+    </section>
     </div>
   );
 }
