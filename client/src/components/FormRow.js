49c3ec58e2a244f9884092da5176c1e574737212
Mike Boyd <mboyd3083@gmail.com>
2023-03-20 15:48:22 -0400

    created component for rows in the form

---
+++ client/src/components/FormRow.js
@@ -0,0 +1,18 @@
+
+const FormRow = ({type,name,value,hangleChange,labelText}) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={hangleChange}
+        className="form-input"
+      />
+    </div>
+  );
+}
+export default FormRow
