7f51f0b28fe113b007be99123a4d9c15a9c26a69
Mike Boyd <mboyd3083@gmail.com>
2023-03-20 15:53:17 -0400

    export pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Landing from "../pages/Landing";
+import Dashboard from "../pages/Dashboard";
+import Register from "../pages/Register";
+import Error from "../pages/Error";
+
+export { Landing, Dashboard, Register, Error };
