8dc28cae510e90730ab3594b4f76c30bcb05d906
Mike Boyd <mboyd3083@gmail.com>
2023-03-22 15:55:47 -0400

    refactored error handler to utilize custom errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,22 @@
-const errorHandlerMiddleware = (err,req,res,next)=>{
-    console.log(err);
-    res.status(500).json({msg: 'there was an error'})
-}
+import { StatusCodes } from "http-status-codes";
 
-export default errorHandlerMiddleware
+const errorHandlerMiddleware = (err, req, res, next) => {
+  const defaultError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "something went wrong try again later",
+  };
+  if (err.name === "ValidationError") {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`;
+  }
+  
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+};
+
+export default errorHandlerMiddleware;
