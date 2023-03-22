f661ea193b9895147828b09c61ec12d848ee006f
Mike Boyd <mboyd3083@gmail.com>
2023-03-22 15:56:24 -0400

    fixed bug and added functionality to hash passwords and create jwt's

--- models/User.js
+++ models/User.js
@@ -1,40 +1,54 @@
-import mongoose from 'mongoose';
-import validator from 'validator';
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        require:[true,'Please provide name'],
-        minlength:3,
-        maxlength:20,
-        trim:true,
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
     },
-    email:{
-        type:String,
-        require:[true,'Please provide email'],
-        validate:{
-            validators:validator.isEmail,
-            message:'Please provide a valid email'
-        },
-        unique:true,
-    },
-    password:{
-        type:String,
-        require:[true,'Please provide password'],
-        minlength:6,
-    },
-    lastName:{
-        type:String,
-        trim:true,
-        maxlength:20,
-        default:'lastName'
-    },
-    location:{
-        type:String,
-        trim:true,
-        maxlength:20,
-        default:'my city',
-    },
-})
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+    select: false,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    trim: true,
+    maxlength: 20,
+    default: "my city",
+  },
+});
 
-export default mongoose.model('User',UserSchema);
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
+
+export default mongoose.model("User", UserSchema);
