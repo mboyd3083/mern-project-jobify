8e9863badbcab197875445261c051ce4831e58ba
Mike Boyd <mboyd3083@gmail.com>
2023-03-22 15:53:54 -0400

    setup functionality to register user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,41 @@
-const register = (req,res) =>{
-    res.send('register')
-}
-const login = (req,res) =>{
-    res.send('login')
-}
-const updateUser = (req,res) =>{
-    res.send('update user')
-}
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 
-export {register,login,updateUser}
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("please provide all values");
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({
+      user: {
+        email: user.email,
+        lastName: user.lastName,
+        location: user.location,
+        name: user.name,
+      },
+      token,
+      location: user.location,
+    });
+};
+
+const login = (req, res) => {
+  res.send("login");
+};
+const updateUser = (req, res) => {
+  res.send("update user");
+};
+
+export { register, login, updateUser };
