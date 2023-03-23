90ed6af900b3d36078186b82cc45f72edaeb8204
Mike Boyd <mboyd3083@gmail.com>
2023-03-23 19:59:31 -0400

    removed Dashboard import

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,5 @@
 import Landing from "../pages/Landing";
-import Dashboard from "../pages/Dashboard";
 import Register from "../pages/Register";
 import Error from "../pages/Error";
 
-export { Landing, Dashboard, Register, Error };
+export { Landing, Register, Error };
