1c46cedd3faa50b74d0bba0b8ca2dd4fd57c239b
Mike Boyd <mboyd3083@gmail.com>
2023-03-23 15:52:48 -0400

    updated

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,14 +1,29 @@
 import React, { useReducer, useContext } from "react";
 
 import reducer from "./reducer";
+import axios from "axios";
 
-import { DISPLAY_ALERT, CLEAR_ALERT } from "./actions";
+import {
+  DISPLAY_ALERT,
+  CLEAR_ALERT,
+  SETUP_USER_BEGIN,
+  SETUP_USER_SUCCESS,
+  SETUP_USER_ERROR,
+} from "./actions";
+
+const token = localStorage.getItem("token");
+const user = localStorage.getItem("user");
+const userLocation = localStorage.getItem("location");
 
 const initialState = {
   isLoading: false,
   showAlert: false,
   alertText: "",
   alertType: "",
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  useLocation: userLocation || "",
+  jobLocation: userLocation || "",
 };
 
 const AppContext = React.createContext();
@@ -27,11 +42,47 @@ const AppProvider = ({ children }) => {
     }, 3000);
   };
 
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
+  const removeUserToLocalStorage = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    localStorage.removeItem("location");
+  };
+
+  const setupUser = async ({currentUser, endPoint, alertText}) => {
+    console.log(endPoint);
+    dispatch({ type: SETUP_USER_BEGIN });
+    try {
+      const { data } = await axios.post(
+        `/api/v1/auth/${endPoint}`,
+        currentUser
+      );
+      const { user, token, location } = data;
+      console.log(user);
+      dispatch({
+        type: SETUP_USER_SUCCESS,
+        payload: { user, token, location, alertText },
+      });
+      addUserToLocalStorage({ user, token, location });
+    } catch (error) {
+      dispatch({
+        type: SETUP_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
+  };
+
   return (
     <AppContext.Provider
       value={{
         ...state,
         displayAlert,
+        setupUser,
       }}
     >
       {children}
