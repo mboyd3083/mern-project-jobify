aabf44e1c9414082d2ac0d02c13b529e74afc872
Mike Boyd <mboyd3083@gmail.com>
2023-03-23 19:58:23 -0400

    fixed typo

--- client/src/components/FormRow.js
+++ client/src/components/FormRow.js
@@ -1,5 +1,5 @@
 
-const FormRow = ({type,name,value,hangleChange,labelText}) => {
+const FormRow = ({type,name,value,handleChange,labelText}) => {
   return (
     <div className="form-row">
       <label htmlFor={name} className="form-label">
@@ -9,7 +9,7 @@ const FormRow = ({type,name,value,hangleChange,labelText}) => {
         type={type}
         value={value}
         name={name}
-        onChange={hangleChange}
+        onChange={handleChange}
         className="form-input"
       />
     </div>
