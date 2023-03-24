cfe37b9c387432b204c6143b605bbaa7f1dacc05
Mike Boyd <mboyd3083@gmail.com>
2023-03-24 12:16:49 -0400

    changed port number

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 9000;
 
 const start = async () => {
   try {
