e43af2dd76ad75f56db5121f0911828180db0b9e
Mike Boyd <mboyd3083@gmail.com>
2023-03-24 19:22:00 -0400

    updated exports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,7 @@
 import Landing from "../pages/Landing";
 import Register from "../pages/Register";
 import Error from "../pages/Error";
+import ProtectedRoute from "./ProtectedRoute";
 
-export { Landing, Register, Error };
+
+export { Landing, Register, Error,ProtectedRoute };
