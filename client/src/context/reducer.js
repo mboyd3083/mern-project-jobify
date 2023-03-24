a18f4218e6c90acf18d06c5fef06c50cef75cc53
Mike Boyd <mboyd3083@gmail.com>
2023-03-24 19:17:54 -0400

    updated reducer

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -4,7 +4,10 @@ import {
   SETUP_USER_BEGIN,
   SETUP_USER_SUCCESS,
   SETUP_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER,
 } from "./actions";
+import { initialState } from "./appContext";
 
 const reducer = (state, action) => {
   if (action.type === DISPLAY_ALERT) {
@@ -36,16 +39,33 @@ const reducer = (state, action) => {
       alertType: "success",
       alertText: action.payload.alertText,
     };
-  } 
+  }
 
-  if(action.type === SETUP_USER_ERROR){
+  if (action.type === SETUP_USER_ERROR) {
     return {
       ...state,
       isLoading: false,
       showAlert: true,
       alertType: "danger",
       alertText: action.payload.msg,
-    }
+    };
+  }
+
+  if (action.type === TOGGLE_SIDEBAR) {
+    return {
+      ...state,
+      showSidebar: !state.showSidebar,
+    };
+  }
+
+  if (action.type === LOGOUT_USER) {
+    return {
+      ...initialState,
+      user: null,
+      token: null,
+      userLocation: "",
+      jobLocation: "",
+    };
   }
 
   throw new Error(`no such action: ${action.type}`);
