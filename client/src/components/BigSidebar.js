3e9e12ee3797f4bb72dc1087f14bad4cef991aeb
Mike Boyd <mboyd3083@gmail.com>
2023-03-27 15:50:12 -0400

    updated to diplay bigsidebar with nav links

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,10 +1,25 @@
-import Wrapper from "../assets/wrappers/BigSidebar"
+import NavLinks from "./NavLinks";
+import Wrapper from "../assets/wrappers/BigSidebar";
+import Logo from "../components/Logo";
+import { useAppContext } from "../context/appContext";
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-        <h4>big sidebar</h4>
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
-  )
-}
-export default BigSidebar
+  );
+};
+export default BigSidebar;
