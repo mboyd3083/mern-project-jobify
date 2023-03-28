5e5bc5cc3a5475f7a969ed0e41e909a62361b84a
Mike Boyd <mboyd3083@gmail.com>
2023-03-28 15:53:24 -0400

    handled functionality for creating job on backend

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,23 +1,30 @@
-const createJob = (req,res) =>{
-    res.send('create job');
-}
-const deleteJob = (req,res) =>{
-    res.send('delete job');
-}
-const getAllJobs = (req,res) =>{
-    res.send('get all jobs');
-}
-const updateJob = (req,res) =>{
-    res.send('update jobs');
-}
-const showStats = (req,res) =>{
-    res.send('show stats');
-}
+import Job from "../models/Job.js";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
 
-export {
-    createJob,
-    deleteJob,
-    getAllJobs,
-    updateJob,
-    showStats
-}
+const createJob = async (req, res) => {
+  const { position, company } = req.body;
+
+  if (!position || !company) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ job });
+};
+const deleteJob = (req, res) => {
+  res.send("delete job");
+};
+const getAllJobs = (req, res) => {
+  res.send("get all jobs");
+};
+const updateJob = (req, res) => {
+  res.send("update jobs");
+};
+const showStats = (req, res) => {
+  res.send("show stats");
+};
+
+export { createJob, deleteJob, getAllJobs, updateJob, showStats };
