dc4661d6f043bfb038addfbc09a8bfc80e073350
Mike Boyd <mboyd3083@gmail.com>
2023-03-31 23:26:05 -0400

    updated reducer

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -14,6 +14,8 @@ import {
   CREATE_JOB_BEGIN,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
 } from "./actions";
 import { initialState } from "./appContext";
 
@@ -93,7 +95,7 @@ const reducer = (state, action) => {
       alertText: "User Profile Updated",
     };
   }
-  
+
   if (action.type === UPDATE_USER_ERROR) {
     return {
       ...state,
@@ -104,28 +106,27 @@ const reducer = (state, action) => {
     };
   }
 
-  if(action.type === HANDLE_CHANGE){
+  if (action.type === HANDLE_CHANGE) {
     return {
       ...state,
       [action.payload.name]: action.payload.value,
-    }
+    };
   }
 
-  if(action.type === CLEAR_VALUES){
+  if (action.type === CLEAR_VALUES) {
     const initialState = {
       isEditing: false,
       editJobId: "",
       position: "",
       company: "",
-      jobLocation: state.userLocation || '',
+      jobLocation: state.userLocation || "",
       jobType: "full-time",
       status: "pending",
-
     };
     return {
       ...state,
-      ...initialState
-    }
+      ...initialState,
+    };
   }
 
   if (action.type === CREATE_JOB_BEGIN) {
@@ -139,7 +140,7 @@ const reducer = (state, action) => {
       showAlert: true,
       alertType: "success",
       alertText: "New Job Created",
-    }
+    };
   }
 
   if (action.type === CREATE_JOB_ERROR) {
@@ -151,6 +152,23 @@ const reducer = (state, action) => {
       alertText: action.payload.msg,
     };
   }
+
+  if (action.type === GET_JOBS_BEGIN) {
+    return { ...state, isLoading: true,showAlert:false };
+  }
+
+
+  if (action.type === GET_JOBS_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      jobs:action.payload.jobs,
+      totalJobs:action.payload.totalJobs,
+      numOfPages:action.payload.numOfPages,
+
+    };
+  }
+
   throw new Error(`no such action: ${action.type}`);
 };
 
