687d8f9bd9f2dcf783d20497391cccecdcd55a0d
Mike Boyd <mboyd3083@gmail.com>
2023-04-04 12:01:38 -0400

    updated functionality for update and delete job controllers

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,12 @@
 import Job from "../models/Job.js";
 import { StatusCodes } from "http-status-codes";
-import { BadRequestError, UnAuthenticatedError } from "../errors/index.js";
+import {
+  BadRequestError,
+  UnAuthenticatedError,
+  NotFoundError,
+} from "../errors/index.js";
+import checkPermissions from "../utils/checkPermissions.js";
+import mongoose from 'mongoose';
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -14,19 +20,60 @@ const createJob = async (req, res) => {
 
   res.status(StatusCodes.CREATED).json({ job });
 };
-const deleteJob = (req, res) => {
-  res.send("delete job");
+
+const deleteJob = async (req, res) => {
+  const { id: jobId } = req.params;
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  await job.deleteOne();
+
+  res.status(StatusCodes.OK).json({ msg: "Success! job removed" });
 };
-const getAllJobs = async(req, res) => {
-  const jobs = await Job.find({createdBy: req.user.userId});
-  
-  res.status(StatusCodes.OK).json({jobs, totalJobs:jobs.length, numOfPages:1});
+
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user.userId });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ jobs, totalJobs: jobs.length, numOfPages: 1 });
 };
-const updateJob = (req, res) => {
-  res.send("update jobs");
+const updateJob = async (req, res) => {
+  const { id: jobId } = req.params;
+
+  const { company, position } = req.body;
+  if (!company || !position) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const job = await Job.findOne({ _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id :${jobId}`);
+  }
+
+  checkPermissions(req.user, job.createdBy);
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(StatusCodes.OK).json({ updatedJob });
 };
-const showStats = (req, res) => {
-  res.send("show stats");
+
+const showStats = async(req, res) => {
+  let stats = await Job.aggregate([
+    {$match : {createdBy: mongoose.Types.ObjectId(req.user.userId)}},
+    
+  ])
+  res.status(StatusCodes.OK).json({stats});
 };
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats };
