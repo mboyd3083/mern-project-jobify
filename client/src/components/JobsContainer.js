79ef56332101b32bab05a512324c08ea5d33ff70
Mike Boyd <mboyd3083@gmail.com>
2023-04-05 15:48:02 -0400

    updated useEffect dependencies

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -5,34 +5,45 @@ import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
 
 const JobsContainer = () => {
-  const {getJobs,jobs,isLoading,page, totalJobs} = useAppContext();
+  const {
+    getJobs,
+    jobs,
+    isLoading,
+    page,
+    totalJobs,
+    search,
+    searchStatus,
+    searchType,
+    sort,
+  } = useAppContext();
 
-  useEffect(()=>{
+  useEffect(() => {
     getJobs();
-  }, [])
+  }, [search, searchStatus, searchType, sort]);
 
-  if(isLoading){
-    return <Loading/>
+  if (isLoading) {
+    return <Loading />;
   }
 
-  if(jobs.length === 0){
-    return <Wrapper>
-      <h2>No jobs to display...</h2>
-    </Wrapper>
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
   }
 
-  return(
+  return (
     <Wrapper>
-      <h5>{totalJobs} job{jobs.length > 1 && 's'} found</h5>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
       <div className="jobs">
-        {
-          jobs.map((job)=>{
-            
-            return <Job key={job._id} {...job}/>
-          })
-        }
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
       </div>
     </Wrapper>
-    )
+  );
 };
 export default JobsContainer;
