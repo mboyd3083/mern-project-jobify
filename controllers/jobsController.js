9385b7b236d75d1da91718bfc9abc2a42feb2dbe
Mike Boyd <mboyd3083@gmail.com>
2023-04-05 13:02:51 -0400

    updated controller to handle functionality for show stats requests

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -6,7 +6,8 @@ import {
   NotFoundError,
 } from "../errors/index.js";
 import checkPermissions from "../utils/checkPermissions.js";
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import moment from "moment";
 
 const createJob = async (req, res) => {
   const { position, company } = req.body;
@@ -68,12 +69,51 @@ const updateJob = async (req, res) => {
   res.status(StatusCodes.OK).json({ updatedJob });
 };
 
-const showStats = async(req, res) => {
+const showStats = async (req, res) => {
   let stats = await Job.aggregate([
-    {$match : {createdBy: mongoose.Types.ObjectId(req.user.userId)}},
-    
-  ])
-  res.status(StatusCodes.OK).json({stats});
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    {
+      $group: {
+        _id: { year: { $year: "$createdAt" }, month: { $month: "$createdAt" } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { "_id.year": -1, "_id.month": -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format("MMM Y");
+      return { date, count };
+    })
+    .reverse();
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats };
