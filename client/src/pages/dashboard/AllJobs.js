f6b7254b6b88dff3d8e33bcee3ffeb9c577c76c4
Mike Boyd <mboyd3083@gmail.com>
2023-04-11 11:06:14 -0400

    fixed warnings

--- client/src/pages/dashboard/AllJobs.js
+++ client/src/pages/dashboard/AllJobs.js
@@ -1,7 +1,5 @@
-import { useAppContext } from "../../context/appContext";
 import { SearchContainer, JobsContainer } from "../../components";
 const AllJobs = () => {
-  const { getJobs } = useAppContext();
   return (
     <>
       <SearchContainer />
