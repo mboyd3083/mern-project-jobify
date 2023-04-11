7f8896c61380e95549a88b6bb624a11c8cad79af
Mike Boyd <mboyd3083@gmail.com>
2023-04-11 11:14:41 -0400

    added security packages as well as served static assets

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ import dotenv from "dotenv";
 dotenv.config();
 import morgan from "morgan";
 
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+
 //db and authenticateUser
 import connectDB from "./db/connect.js";
 
@@ -20,14 +28,22 @@ import authenticateUser from "./middleware/auth.js";
 if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
 }
-app.use(express.json());
 
-app.get("/api/v1", (req, res) => {
-  res.send({ msg: "api" });
-});
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.use(express.json());
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
