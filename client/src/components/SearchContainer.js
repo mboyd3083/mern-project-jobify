536ba0e4fba27032e5b152f580ea0b0b38a74765
Mike Boyd <mboyd3083@gmail.com>
2023-04-11 11:01:16 -0400

    updated to search functionality to use debouncing

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,11 +1,12 @@
 import { FormRow, FormRowSelect } from ".";
 import { useAppContext } from "../context/appContext";
 import Wrapper from "../assets/wrappers/SearchContainer";
-
+import { useState, useMemo } from "react";
 const SearchContainer = () => {
+  const [localSearch, setLocalSearch] = useState("");
   const {
     isLoading,
-    search,
+    // search,
     searchStatus,
     searchType,
     sort,
@@ -17,14 +18,31 @@ const SearchContainer = () => {
   } = useAppContext();
 
   const handleSearch = (e) => {
-    if (isLoading) return;
     handleChange({ name: e.target.name, value: e.target.value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setLocalSearch("");
     clearFilters();
   };
+
+  const debounce = () => {
+    let timeoutID;
+    return (e) => {
+      setLocalSearch(e.target.value);
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        handleChange({ name: e.target.name, value: e.target.value });
+      }, 1000);
+    };
+  };
+
+  const optimizedDebounce = useMemo(
+    () => debounce(),
+    // eslint-disable-next-line
+    []
+  );
 
   return (
     <Wrapper>
@@ -34,8 +52,8 @@ const SearchContainer = () => {
           <FormRow
             type="text"
             name="search"
-            value={search}
-            handleChange={handleSearch}
+            value={localSearch}
+            handleChange={optimizedDebounce}
           />
           <FormRowSelect
             labelText="status"
@@ -57,7 +75,11 @@ const SearchContainer = () => {
             handleChange={handleSearch}
             list={sortOptions}
           />
-          <button className="btn btn-block btn-danger" disabled={isLoading} onClick={handleSubmit}>
+          <button
+            className="btn btn-block btn-danger"
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
             {" "}
             clear filters
           </button>
